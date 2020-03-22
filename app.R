544728300789f0634fc5e23643df15bab2d19be0
Felix Reinbott <Felix.Ka@web.de>
2020-03-22 16:00:59 +0100

    updating about the website

--- app.R
+++ app.R
@@ -379,42 +379,28 @@ ui <- navbarPage(theme = shinytheme("flatly"), collapsible = TRUE,
                            
                  tabPanel("Ueber diese Webseite",
                           tags$div(
-                            tags$h4("Last update"), 
-                            h6(paste0(update)),
-                            "This site is updated once daily. At this time of rapid escalation of the COVID-19 pandemic, the following resources offer the latest numbers of known cases:",tags$br(),
-                            tags$a(href="https://experience.arcgis.com/experience/685d0ace521648f8a5beeeee1b9125cd", "WHO COVID-19 dashboard"),tags$br(),
-                            tags$a(href="https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6", "Johns Hopkins University COVID-19 dashboard"),tags$br(),
-                            "The aim of this site is to complement the above resources by providing several interactive features not currently available elsewhere, including the timeline function, 
-                            the ability to overlay past outbreaks, and an emphasis on normalised counts (per 100,000 individuals).",tags$br(),
-                            tags$br(),tags$h4("Background"), 
-                            "In December 2019, cases of severe respiratory illness began to be reported across the city of Wuhan in China. 
-                            These were caused by a new type of coronavirus, and the disease is now commonly referred to as COVID-19.
-                            The number of COVID-19 cases started to escalate more quickly in mid-January and the virus soon spread beyond China's borders. 
-                            This story has been rapidly evolving ever since, and each day we are faced by worrying headlines regarding the current state of the outbreak.",
-                            tags$br(),tags$br(),
-                            "In isolation, these headlines can be hard to interpret. 
-                            How fast is the virus spreading? Are efforts to control the disease working? How does the situation compare with previous epidemics?
-                            This site is updated daily based on data published by Johns Hopkins University. 
-                            By looking beyond the headlines, we hope it is possible to get a deeper understanding of this unfolding pandemic.",
-                            tags$br(),tags$br(),
-                            "An article discussing this site was published in ",tags$a(href="https://theconversation.com/coronavirus-outbreak-a-new-mapping-tool-that-lets-you-scroll-through-timeline-131422", "The Conversation. "),
-                            "The map was also featured on the BBC World Service program",tags$a(href="https://www.bbc.co.uk/programmes/w3csym33", "Science in Action."),
-                            tags$br(),tags$br(),tags$h4("Code"),
-                            "Code and input data used to generate this Shiny mapping tool are available on ",tags$a(href="https://github.com/eparker12/nCoV_tracker", "Github."),
-                            tags$br(),tags$br(),tags$h4("Sources"),
-                            tags$b("2019-COVID cases: "), tags$a(href="https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series", "Johns Hopkins Center for Systems Science and Engineering github page,")," with additional information from the ",tags$a(href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports", "WHO's COVID-19 situation reports."),
-                            " In previous versions of this site (up to 17th March 2020), updates were based solely on the WHO's situation reports.",tags$br(),
-                             tags$a(href="https://www.who.int/csr/disease/swineflu/updates/en/", "WHO situation reports"),tags$br(),
-                            "Model estimates from ", tags$a(href="https://journals.plos.org/plosmedicine/article?id=10.1371/journal.pmed.1001558", "GLaMOR Project"),tags$br(),
+                            tags$h4("Idee und Impact"),
+                            "Dieses Projekt ist ein proof of concept fuer das Agreggieren und Visualisieren der Betreuungssituation fuer Corona Patienten. Der Impact einer solchen Anwendung ist es Krankenhauesern ein Tool zu geben, woraus ersichtlich ist welche Einrichtungen im Umkreis freie Kapazitaeten haben und somit die Auslastung der Krankenhaeuser effizienter gestalten zu koennen. Ebenso soll es somit erleichtert werden, Krisengebiete schnell auf der Karte sichtbar zu machen und durch entsprechende Erweiterungen mithilfe von Predictive Modelling zukuenftige Kriesengebiete fruhzeitig zu erkennen. Somit wird die rechtzeitige Planung entsprechender Massnahmen effektiver erhoffen wir uns.",tags$br(),
                             tags$br(),
+                            tags$h4("Umsetzung"),
+                            "Aufgrund der Tatsache, dass wir ein zwei Personen Team sind und somit unsere Ressourcen eingeschraenkt sind haben wir begonnen mit einem Fork des Dashboards:",tags$br(),
+                            tags$a(href="https://github.com/eparker12/nCoV_tracker", "https://github.com/eparker12/nCoV_tracker"), 
+                            tags$br(), tags$br(),
+                            "und konzentrierten uns darauf die Funktionalitaet fuer das Sammeln von Daten von Krankenhaeusern zu erweitern. Insbesondere ist es hier besonders einfach in R entwickelte Vorhersagetools einzubinden und somit auch beliebig komplexe vorhersagen einbinden zu koennen. Ebenso ist eine Anbindung an Datenbanken und ein schnelles Deplayment auf Servern moeglich. Insbesondere lassen sich schnell nneue Tools in das Framework einbinden.",
+                            tags$br(),tags$br(),
+
+                            tags$h4("Referenzen"),
+                            "Karte ",tags$a(href="https://www.bbc.co.uk/programmes/w3csym33", "Science in Action."),
+                            "Daten ",tags$a(href="https://github.com/eparker12/nCoV_tracker", "Github."),
+                            tags$a(href="https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series", "Johns Hopkins Center for Systems Science and Engineering github page,"), tags$a(href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports", "WHO's COVID-19 situation reports."),
+                             tags$a(href="https://www.who.int/csr/disease/swineflu/updates/en/", "WHO situation reports"),tags$br(), 
+                            tags$a(href="https://gist.github.com/tadast/8827699", "Github"),tags$br(),
+                            tags$br(),tags$br(),tags$h4("Authoren"),
+                            "Felix Reinbott & Borui Zhu",
                             tags$br(),
-                            tags$b("Country mapping coordinates: "), tags$a(href="https://gist.github.com/tadast/8827699", "Github"),tags$br(),
-                            tags$br(),tags$br(),tags$h4("Authors"),
+                            tags$h4("Authoren des Forks"),
                             "Dr Edward Parker, The Vaccine Centre, London School of Hygiene & Tropical Medicine",tags$br(),
-                            "Quentin Leclerc, Department of Infectious Disease Epidemiology, London School of Hygiene & Tropical Medicine",tags$br(),
-                            tags$br(),tags$br(),tags$h4("Contact"),
-                            "edward.parker@lshtm.ac.uk",tags$br(),tags$br(),
-                            tags$img(src = "vac_dark.png", width = "150px", height = "75px"), tags$img(src = "lshtm_dark.png", width = "150px", height = "75px")
+                            "Quentin Leclerc, Department of Infectious Disease Epidemiology, London School of Hygiene & Tropical Medicine",tags$br()
                           )
                  )
 )
@@ -528,7 +514,7 @@ server = function(input, output) {
                    style = list("font-weight" = "normal", padding = "3px 8px", "color" = covid_col),
                    textsize = "15px", direction = "auto")) %>%
 
-      addCircles(data = reactive_betten_db(), lat = ~ lat, lng = ~ lon, weight = 1, radius = ~(Anzahl.Betten)^(1/4)*1.5e4, fillOpacity = 0.1, color = betten_col, group = "Anzahl Betten", labelOptions = labelOptions(style = list("font-weight" = "normal", padding = "3px 8px", "color" = betten_col), textsize = "15px", direction = "auto")) 
+      addCircles(data = reactive_betten_db(), lat = ~ lat, lng = ~ lon, weight = 1, radius = ~(Anzahl.Betten)^(1/4)*9.5e3, fillOpacity = 0.1, color = betten_col, group = "Anzahl Betten", labelOptions = labelOptions(style = list("font-weight" = "normal", padding = "3px 8px", "color" = betten_col), textsize = "15px", direction = "auto")) 
 
   })
   
